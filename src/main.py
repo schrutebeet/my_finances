38bc0be729499afb691caef8bb41304f129363fb
schrutebeet <ricardosans98@gmail.com>
2024-10-23 23:47:58 -0400

    Add a temporary main file from where to work

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
+from pathlib import Path
+
 from src.banking.ubs_transactions_reader import UBSTrasactionReader
 
 
 if __name__ == "__main__":
-    UBSTrasactionReader(r"C:\Users\Ricky\Downloads\september_2024.csv")
+    ubs_trx_path = Path("/Users/ricky/Downloads/ubs_september_24.csv")
+    cat_path = Path("/Users/ricky/Documents/GitHub/my_finances/config/ubs_expenditure_cat.yaml")
+    ubs_trxs = UBSTrasactionReader(ubs_trx_path, cat_path)
+    ubs_trxs_df = ubs_trxs.get_clean_trx_df()
+    ubs_trxs.categorize_trxs(ubs_trxs_df)
